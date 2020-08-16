1acb38357d2b14bfd1d9c0547b25f0db5b5f518f
LianxinGao <503773453@qq.com>
2020-08-16 22:18:46 +0800

    update learn

---
+++ Scala相关内容/src/ListMethodTest.scala
@@ -0,0 +1,19 @@
+object ListMethodTest {
+  def main(args: Array[String]): Unit = {
+//    var lst = List("Avril", "Lavigne", "Kirito", "Asuna", "Lisa", "Violet")
+//    lst = "Names:" :: lst
+//    println(lst)
+//    println(lst.tail) // 返回除首个元素的列表
+//    println(lst.init) // 返回除最后一个元素的列表
+//    println(lst.dropRight(2))
+//    println(lst.count(s => s.length == 5))
+//    println(lst.filter(s => s.length == 5))
+//    println(lst.mkString(" "))
+//    println(lst.sortWith((s, t) => s.toLowerCase() < t.toLowerCase()))
+//    println("===========")
+//    println(lst.apply(1))
+    println(printFormate(Array("a", "b", "c")))
+  }
+  def printFormate(args:Array[String]) = args.mkString("\n")
+
+}
